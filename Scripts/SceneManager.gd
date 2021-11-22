2cbd38966c256cea097b1c2010dfd875756fb308
Vinimay Kaul <vinimaykaul@gmail.com>
2021-11-22 22:34:25 +0700

    Adds Comments to the SceneManager class

--- Scripts/SceneManager.gd
+++ Scripts/SceneManager.gd
@@ -3,9 +3,11 @@ extends Spatial
 class_name SceneManager
 
 func _ready():
+#	Set the mouse mode to captured on game start
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
 func _process(delta):
+#	Alternate mouse mode from captured to visible each time "ESC" is pressed
 	if (Input.is_action_just_pressed("ui_cancel")):
 		if (Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED):
 			Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
